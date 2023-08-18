c8b3f28490ab2fa8a8907fcd594720d5249ae56d
uzohou <isaiahntina47@gmail.com>
2023-08-18 20:57:43 +0100

    new build in action

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,36 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    main: './index.js' // Update this entry point accordingly
+  },
   output: {
-    filename:'src/[name].bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template:"./public/index.html",
-      filename: 'public/index.html',
+      template: "./public/index.html",
+      filename: 'index.html', // No need to specify the 'public' directory here
     }),
     new HtmlWebpackPlugin({
-      template:"./public/search.html",
-      filename: 'public/search.html',
+      template: "./public/search.html",
+      filename: 'search.html', // No need to specify the 'public' directory here
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'css', // Source directory
+          from: 'public/css', // Source directory
           to: 'css',   // Destination directory in 'dist'
         },
       ],
     }),
-
     new MiniCssExtractPlugin({
       filename: '[name].css'
     })
   ],
-  
   module: {
     rules: [
       {
@@ -52,7 +51,6 @@ module.exports = {
       }
     ]
   },
- 
   resolve: {
     fallback: {
       "crypto": false,
@@ -68,7 +66,6 @@ module.exports = {
       "os": false,
       "assert": false,
       "async_hooks": false
-
     }
   },
   devServer: {
@@ -83,4 +80,90 @@ module.exports = {
     }
   }
 };
+
+// const path = require('path');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+// module.exports = {
+//   entry: 'server.js',
+//   output: {
+//     filename:'[name].bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     clean: true,
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template:"./public/index.html",
+//       filename: 'public/index.html',
+//     }),
+//     new HtmlWebpackPlugin({
+//       template:"./public/search.html",
+//       filename: 'public/search.html',
+//     }),
+//     new CopyWebpackPlugin({
+//       patterns: [
+//         {
+//           from: 'css', // Source directory
+//           to: 'css',   // Destination directory in 'dist'
+//         },
+//       ],
+//     }),
+
+//     new MiniCssExtractPlugin({
+//       filename: '[name].css'
+//     })
+//   ],
+  
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env']
+//           }
+//         }
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [MiniCssExtractPlugin.loader, 'css-loader']
+//       }
+//     ]
+//   },
+ 
+//   resolve: {
+//     fallback: {
+      // "crypto": false,
+      // "fs": false,
+      // "stream": false,
+      // "zlib": false,
+      // "http": false,
+      // "net": false,
+      // "querystring": false,
+      // "url": false,
+      // "path": false,
+      // "buffer": false,
+      // "os": false,
+      // "assert": false,
+      // "async_hooks": false
+
+//     }
+//   },
+//   devServer: {
+//     contentBase: path.join(__dirname, 'public'),
+//     compress: true,
+//     port: 300
+//   },
+//   mode: 'production',
+//   optimization: {
+//     splitChunks: {
+//       chunks: 'all'
+//     }
+//   }
+// };
 

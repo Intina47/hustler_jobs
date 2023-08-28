911c83a62547c514ba29da516680c3ad6ee76ca1
uzohou <isaiahntina47@gmail.com>
2023-08-28 16:06:09 +0100

    controlled format string

--- public/src/jobDetailsController.js
+++ public/src/jobDetailsController.js
@@ -3,21 +3,20 @@ const fs = require('fs');
 const getJobDetails = (req, res) => {
     console << "System Request!"
     const fileName = req.params.fileName;
+    let ip = req.connection.remoteAddress;
     const filePath = path.join(__dirname, '..','assets', 'pdf_files', fileName);
     fs.stat(filePath, function(err){
       if (err == null) {
-        console.log('File exists');
         // Set the appropriate content type for PDF files
         res.contentType('application/pdf');
-        // Send the PDF file
-        console.log("------------------Loading file--------------: ", filePath);
+        console.log(ip, "---------sys request success, Loading file");
         res.sendFile(filePath);
       } else if (err.code === 'ENOENT') {
-        console.log(fileName, ' does not exist');
+        console.log('Unauthorized access attempt by ', ip);
         res.status(404).send('File not found');
       } else {
         console.log('Some other error: ', err.code);
-        throw new Error(`Error ${err}`);
+        res.end();
       }
     });
 }

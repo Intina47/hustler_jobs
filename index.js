722edbaee818cd9f564a055c493a2623d454557d
uzohou <isaiahntina47@gmail.com>
2023-08-28 17:03:42 +0100

    rate limiter

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    delayMs: 0 // disable delaying - full speed until the max limit is reached
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(limiter);
 
 const { getJobs } = require('./public/src/jobController.js');
 const { getJobDetails } = require('./public/src/jobDetailsController.js');
